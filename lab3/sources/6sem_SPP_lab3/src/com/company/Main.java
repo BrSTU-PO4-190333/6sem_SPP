ec54ab71da82e0960489ab4f5bc5a7488a587c01
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-03 03:11:40 +0300

    lab3/Galanin_Pavel : Added menu

--- lab3/sources/6sem_SPP_lab3/src/com/company/Main.java
+++ lab3/sources/6sem_SPP_lab3/src/com/company/Main.java
@@ -3,26 +3,98 @@ package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
     protected static Connection connection;
+    protected static Statement statement;
 
     public static void main(String[] args) {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:mydatabase.sqlite");
             System.out.println("Success connect to database");
-            Statement statement = connection.createStatement();
-            RB_Nomenclature.create_table(statement);
-            RB_Nomenclature.create(statement, "red mouse");
-            RB_Nomenclature.create(statement, "green mouse");
-            RB_Nomenclature.read(statement);
-            RB_Nomenclature.update(statement, 1, "grey mouse");
-            RB_Nomenclature.delete_(statement, 2);
-            RB_Nomenclature.read(statement);
-            RB_Nomenclature.drop_table(statement);
+            statement = connection.createStatement();
         }
         catch (Exception e) {
             System.out.println(e);
+            return;
         }
+        init_database();
+        main_menu();
+    }
+
+    // Функция, которая создает таблицы в базе данных
+    protected static void init_database() {
+        RB_Nomenclature.create_table(statement);
+    }
+
+    // Функция, которая вызывает главное меню
+    protected static void main_menu() {
+        String key;
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            print_main_menu();
+            key = in.nextLine();
+
+            if (key.equals("0")) {
+                break;
+            }
+
+            if (key.equals("1")) {
+                RB_Nomenclature.create_interface(statement);
+                press_any_key();
+                continue;
+            }
+
+            if (key.equals("2")) {
+                RB_Nomenclature.read(statement);
+                press_any_key();
+                continue;
+            }
+
+            if (key.equals("3")) {
+                RB_Nomenclature.update_interface(statement);
+                press_any_key();
+                continue;
+            }
+
+            if (key.equals("4")) {
+                RB_Nomenclature.delete_interface(statement);
+                press_any_key();
+                continue;
+            }
+
+            if (key.equals("7")) {
+                RB_Nomenclature.create_table(statement);
+                press_any_key();
+                continue;
+            }
+
+            if (key.equals("8")) {
+                RB_Nomenclature.drop_table(statement);
+                press_any_key();
+                continue;
+            }
+        }
+    }
+
+    // Функция, которая печатает меню
+    protected static void print_main_menu() {
+        System.out.println("Menu:");
+        System.out.println("1. Create element");
+        System.out.println("2. Read table");
+        System.out.println("3. Update element");
+        System.out.println("4. Delete element");
+        System.out.println("7. !!! Create database!!!");
+        System.out.println("8. !!! Drop database!!!");
+        System.out.println("0. Exit");
+        System.out.println("");
+    }
+
+    // Функция, которая ожидает ввода пользователя
+    protected static void press_any_key() {
+        System.out.print("Press any key... ");
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
     }
 }
--- lab3/sources/6sem_SPP_lab3/src/com/company/RB_Nomenclature.java
+++ lab3/sources/6sem_SPP_lab3/src/com/company/RB_Nomenclature.java
@@ -2,11 +2,13 @@ package com.company;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class RB_Nomenclature {
     //protected int CodeNomenclature;
     //protected String Name;
 
+    // Функция, которая создает таблицу
     protected static void create_table(Statement statement) {
         final String sql = """
             CREATE TABLE IF NOT EXISTS "RB_Nomenclature" (
@@ -22,6 +24,7 @@ public class RB_Nomenclature {
         }
     }
 
+    // Функция, которая добавляет элемент
     protected static void create(Statement statement, String Name) {
         final String sql = "INSERT INTO \"RB_Nomenclature\" (Name) VALUES (\"" + Name + "\");";
         try {
@@ -32,10 +35,22 @@ public class RB_Nomenclature {
         }
     }
 
+    // Функция, которая позволяет добавлять элемент через консоль
+    protected static void create_interface(Statement statement) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n = = = = = = = =");
+        System.out.println("Create nomenclature element");
+        System.out.print("Name: ");
+        String Name = in.nextLine();
+        create(statement, Name);
+    }
+
+    // Функция, которая выводит таблицу в консоль
     protected static void read(Statement statement) {
         final String sql = "SELECT * FROM \"RB_Nomenclature\"";
         try {
             ResultSet result_set = statement.executeQuery(sql);
+            System.out.println("\n = = = = = = = =");
             System.out.println(" Table - RB_Nomenclature");
             System.out.printf("| %-8s | %-64s | \n", "#", "Name");
             System.out.printf("| %-8s | %-64s | \n",
@@ -52,7 +67,8 @@ public class RB_Nomenclature {
         }
     }
 
-    protected static void update(Statement statement, int id, String Name) {
+    // Функция, которая обновляет запись по id
+    protected static void update(Statement statement, String id, String Name) {
         final String sql = "UPDATE \"RB_Nomenclature\" SET Name = \""
                 + Name + "\" WHERE CodeNomenclature = " + id + ";";
         try {
@@ -63,7 +79,23 @@ public class RB_Nomenclature {
         }
     }
 
-    protected static void delete_(Statement statement, int id) {
+    // Функция, которая обновляет запись через консоль
+    protected static void update_interface(Statement statement) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n = = = = = = = =");
+        System.out.println("Update nomenclature element");
+
+        System.out.print("id: ");
+        String id = in.nextLine();
+
+        System.out.print("Name: ");
+        String Name = in.nextLine();
+
+        update(statement, id, Name);
+    }
+
+    // Функция, которая удаляет запись по id
+    protected static void delete_(Statement statement, String id) {
         final String sql = "DELETE FROM \"RB_Nomenclature\" WHERE CodeNomenclature = " + id + ";";
         try {
             statement.execute(sql);
@@ -73,6 +105,19 @@ public class RB_Nomenclature {
         }
     }
 
+    // Функция, которая удаляет запись через консоль
+    protected static void delete_interface(Statement statement) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n = = = = = = = =");
+        System.out.println("Delete nomenclature element");
+
+        System.out.print("id: ");
+        String id = in.nextLine();
+
+        delete_(statement, id);
+    }
+
+    // Функция, которая удаляет таблицу
     protected static void drop_table(Statement statement) {
         final String sql = "DROP TABLE \"RB_Nomenclature\";";
         try {
