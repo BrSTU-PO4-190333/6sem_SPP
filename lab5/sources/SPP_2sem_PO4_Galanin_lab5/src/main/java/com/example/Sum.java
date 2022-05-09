f6fdeb676809f8353417decc4f14d2e3aa428163
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-09 11:33:53 +0300

    lab5/Galanin_Pavel : Updated SumTest

--- lab5/sources/SPP_2sem_PO4_Galanin_lab5/src/main/java/com/example/Sum.java
+++ lab5/sources/SPP_2sem_PO4_Galanin_lab5/src/main/java/com/example/Sum.java
@@ -1,8 +1,16 @@
 package com.example;
 
 public class Sum {
-    public static int accum(int ... values) {
+    public static int accum(int... values) {
         int result = 0;
+        for (int i = 0; i < values.length; i++) {
+            result += values[i];
+        }
+        return result;
+    }
+
+    public static long accum(long... values) {
+        long result = 0;
         for (int i = 0; i < values.length; i++) {
             result += values[i];
         }
--- lab5/sources/SPP_2sem_PO4_Galanin_lab5/src/test/java/com/example/SumTest.java
+++ lab5/sources/SPP_2sem_PO4_Galanin_lab5/src/test/java/com/example/SumTest.java
@@ -6,12 +6,27 @@ import static org.junit.Assert.assertEquals;
 
 public class SumTest {
     @Test
-    public void accum() {
-        // Результат реальный
-        int actual = Sum.accum(1, 2,3,4,5,6,7,8,9);
-        // Результат ожидаемый
-        int expected = 45;
-        // Проверка на эквивалентность
-        assertEquals(expected, actual);
+    public void accum__int() {
+        int actual; // Результат реальный
+        int expected; // Результат ожидаемый
+
+        actual = Sum.accum(1, 2,3,4,5,6,7,8,9);
+        expected = 45;
+        assertEquals(expected, actual); // Проверка на эквивалентность
+    }
+
+    @Test
+    public void accum__long() {
+        long actual; // Результат реальный
+        long expected; // Результат ожидаемый
+
+        actual = Sum.accum(1, 2,3,4,5,6,7,8,9);
+        expected = 45;
+        assertEquals(expected, actual); // Проверка на эквивалентность
+
+        long [] arr = new long[] {Integer.MAX_VALUE, 2};
+        actual = Sum.accum(arr);
+        expected = Integer.MAX_VALUE + 2L;
+        assertEquals(expected, actual); // Проверка на эквивалентность
     }
 }
